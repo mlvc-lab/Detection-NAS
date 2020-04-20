3ba8eceed605d28aa72939b4090b83cb435e0eea
wheemyungshin <zerger26@gmail.com>
2020-04-20 17:57:01 +0900

    python3

--- installation.sh
+++ installation.sh
@@ -1,13 +1,13 @@
 # this script installs requirements for the repository
 # based on April.10.2020
 apt-get update
-pip install tensorflow==1.15
-pip install tensorflow-gpu==1.15
+pip3 install tensorflow==1.15
+pip3 install tensorflow-gpu==1.15
 apt-get install -y python-tk
-pip install  Cython matplotlib opencv-python-headless pyyaml Pillow
-pip install  'git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
-pip install  numpy
+pip3 install Cython matplotlib opencv-python-headless pyyaml Pillow
+pip3 install 'git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
+pip3 install numpy
 cd tpu/models
-python setup.py install
+python3 setup.py install
 cd ../..
 apt-get install -y libglib2.0-0
