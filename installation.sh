f5e49b71d8f55083abd9cdf470681fc8c526f219
wheemyungshin <zerger26@gmail.com>
2020-04-21 14:59:47 +0900

    python again

--- installation.sh
+++ installation.sh
@@ -1,13 +1,13 @@
 # this script installs requirements for the repository
 # based on April.10.2020
 apt-get update
-pip3 install tensorflow==1.15
-pip3 install tensorflow-gpu==1.15
+pip install tensorflow==1.15
+pip install tensorflow-gpu==1.15
 apt-get install -y python-tk
-pip3 install Cython matplotlib opencv-python-headless pyyaml Pillow
-pip3 install 'git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
-pip3 install numpy
+pip install Cython matplotlib opencv-python-headless pyyaml Pillow
+pip install 'git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
+pip install numpy
 cd tpu/models
-python3 setup.py install
+python setup.py install
 cd ../..
 apt-get install -y libglib2.0-0
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 MODEL_DIR="tpu/testmodels"
 mkdir $MODEL_DIR
+TFRECORD_FILE="coco/train-00255-of-00256.tfrecord"
 
-python tpu/models/official/detection/main.py \
+python3 tpu/models/official/detection/main.py \
 --user_tpu=False \
 --num_cores=1 \
 --mode=train \
 --model_dir=$MODEL_DIR \
 --eval_after_training=False \
+--training_file_pattern=$TFRECORD_FILE \
 --config_file=tpu/models/official/detection/configs/yaml/retinanet_nasfpn.yaml
