2a170655d5fc8e54b6b8554caa0879da7b01efd0
wheemyungshin <zerger26@gmail.com>
2020-04-21 15:25:29 +0900

    train_file_pattern updated

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ RESNET_DIR="tpu/resnet50/resnet_v1_50.ckpt"
 mkdir $MODEL_DIR
 TFRECORD_FILE="coco/train-00255-of-00256.tfrecord"
 
-python3 tpu/models/official/detection/main.py \
+python tpu/models/official/detection/main.py \
 --user_tpu=False \
 --num_cores=1 \
 --mode=train \
 --model_dir=$MODEL_DIR \
 --eval_after_training=False \
---training_file_pattern=$TFRECORD_FILE \
+--train_file_pattern=$TFRECORD_FILE \
 --checkpoint=$RESNET_DIR \
 --config_file=tpu/models/official/detection/configs/yaml/retinanet_nasfpn.yaml
